9629e9204f963bc6e067263593738bb233c5e113
JackXu <jackxu1999@hotmail.com>
2021-05-27 18:41:01 +0800

    updated features

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,8 @@
 const chatForm = document.getElementById('chat-form');
 const chatMessages = document.querySelector('.chat-messages');
+const roomName = document.getElementById('room-name');
+const userList = document.getElementById('users');
+
 
 // Get username and room from URL
 const { username, room } = Qs.parse(location.search, {
@@ -11,6 +14,12 @@ const socket = io();
 
 // Join chatroom
 socket.emit('joinRoom', {username, room});
+
+// Get room and users
+socket.on('roomUsers', ( { room, users }) => {
+    outputRoomName(room);
+    outputUsers(users);
+});
 
 
 // Message from server
@@ -46,4 +55,15 @@ div.innerHTML = `<p class="meta">${message.username}<span>${message.time}</span>
        ${message.text}
     </p>`;
     chatMessages.appendChild(div);
+}
+
+
+// Add room name to DOM
+function outputRoomName(room) {
+    roomName.innerText = room;
+}
+
+// Add users to DOM
+function outputUsers(users) {
+    userList.innerHTML = `${users.map(user => `<li>${user.username}</li>`).join('')}`;
 }
