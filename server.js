bbaf4c681f054f49b1c2bc1d5798231c39f43ab8
JackXu <jackxu1999@hotmail.com>
2021-05-27 16:06:04 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+const formatMessage = require('./utils/messages');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+// Set static foler
+app.use(express.static('public'));
+
+const botName = 'chatbot';
+
+// Run when client connects
+io.on('connection', socket => {
+    console.log('new WS Connections...');
+
+    // Welcome current user
+    socket.emit('message', formatMessage(botName, 'Welcome to Chatcord'));
+
+    // Broadcast when a user connects
+    socket.broadcast.emit('message', formatMessage(botName, 'A user has joined the chat'));
+
+    // Runs when client disconnects
+    socket.on('disconnect', () => {
+        io.emit('message', formatMessage(botName, 'A user has left the chat'));
+    })
+
+    // Listen for chatMessage
+    socket.on('chatMessage', (msg) => {
+        io.emit('message', formatMessage('USER', msg));
+    })
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
